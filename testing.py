1306f41bfba72a4ee0abdfb6aeb1cb25e458a5a7
Vaporducky <85695322+Vaporducky@users.noreply.github.com>
2021-10-14 11:09:06 -0500

    Testing file created

---
+++ testing.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 14 09:36:59 2021
+
+@author: Elianther
+"""
+
+import pandas as pd
+import  numpy as np
+
+x       = np.random.normal(1,2,10)
+aSeries = pd.Series(data = x)
